0b17e40e1537d443a9a6dfe0b8f3e659b36f721e
Sarthak Tejale <stejale1@stevens.edu>
2023-04-28 17:36:54 -0400

    changes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,54 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCb4rbFiBxf9zcDR-SJySNci9_cQgoneMU",
+    authDomain: "solarify-b93cc.firebaseapp.com",
+    projectId: "solarify-b93cc",
+    storageBucket: "solarify-b93cc.appspot.com",
+    messagingSenderId: "381957257053",
+    appId: "1:381957257053:web:39711c7870405958b1dab4",
+    measurementId: "G-XF302HK8B1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+// let's code 
+var datab  = firebase.database().ref('data');
+function UserRegister(){
+var email = document.getElementById('eemail').value;
+var password = document.getElementById('lpassword').value;
+firebase.auth().createUserWithEmailAndPassword(email,password).then(function(){
+   
+}).catch(function (error){
+   var errorcode = error.code;
+   var errormsg = error.message;
+});
+}
+const auth = firebase.auth();
+function SignIn(){
+   var email = document.getElementById('eemail').value;
+   var password = document.getElementById('lpassword').value;
+   const promise = auth.signInWithEmailAndPassword(email,password);
+   promise.catch( e => alert(e.msg));
+   window.open("https://www.google.com","_self");
+}
+document.getElementById('form').addEventListener('submit', (e) => {
+   e.preventDefault();
+   var userInfo = datab.push();
+   userInfo.set({
+       name: getId('fname'),
+       email : getId('eemail'),
+       password : getId('lpassword')
+   });
+   alert("Successfully Signed Up");
+   console.log("sent");
+   document.getElementById('form').reset();
+});
+function  getId(id){
+   return document.getElementById(id).value;
+}
+
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
@@ -19,3 +70,122 @@ btnPopup.addEventListener('click', ()=> {
 iconClose.addEventListener('click', ()=> {
     wrapper.classList. remove('active-popup');
 });
+
+// SIDEBAR TOGGLE
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  if(!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if(sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
+
+
+
+// ---------- CHARTS ----------
+
+// BAR CHART
+var barChartOptions = {
+  series: [{
+    data: [10, 8, 6, 4, 2]
+  }],
+  chart: {
+    type: 'bar',
+    height: 350,
+    toolbar: {
+      show: false
+    },
+  },
+  colors: [
+    "#246dec",
+    "#cc3c43",
+    "#367952",
+    "#f5b74f",
+    "#4f35a1"
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: '40%',
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  legend: {
+    show: false
+  },
+  xaxis: {
+    categories: ["Laptop", "Phone", "Monitor", "Headphones", "Camera"],
+  },
+  yaxis: {
+    title: {
+      text: "Count"
+    }
+  }
+};
+
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barChart.render();
+
+
+// AREA CHART
+var areaChartOptions = {
+  series: [{
+    name: 'Purchase Orders',
+    data: [31, 40, 28, 51, 42, 109, 100]
+  }, {
+    name: 'Sales Orders',
+    data: [11, 32, 45, 32, 34, 52, 41]
+  }],
+  chart: {
+    height: 350,
+    type: 'area',
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#4f35a1", "#246dec"],
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+  markers: {
+    size: 0
+  },
+  yaxis: [
+    {
+      title: {
+        text: 'Purchase Orders',
+      },
+    },
+    {
+      opposite: true,
+      title: {
+        text: 'Sales Orders',
+      },
+    },
+  ],
+  tooltip: {
+    shared: true,
+    intersect: false,
+  }
+};
+
+var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+areaChart.render();
